5745ef97d7da5123712c9a31a3f86660bbbbeaa6
Chen Keinan <hen.keinan@gmail.com>
2025-02-09 16:36:26 +0200

    update params
    Signed-off-by: Chen Keinan <hen.keinan@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module runtime-cli
 
 go 1.23.4
+
+require helm.sh/helm v2.17.0+incompatible
+
+require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/cyphar/filepath-securejoin v0.4.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apimachinery v0.32.1 // indirect
+	k8s.io/helm v2.17.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -2,68 +2,138 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
-	// "strings"
+	"strings"
+	"time"
+
+	"helm.sh/helm/pkg/chartutil"
 )
 
 func main() {
-	data := map[string]string{
-		"codefresh.url":             "https://<your-platform-url>.ngrok.io",
-		"codefresh.userToken.token": "",
-		"codefresh.accountId":       "",
-		"runtime.ingressUrl":        "http://host.docker.internal:8080",
-		"service.type":              "NodePort",
-		"service.nodePort":          "31243",
-		"app-proxy.config.cors":     "http://local.codefresh.io,https://<your-platform-url>.ngrok.io",
-	}
-	keys := []string{"codefresh.url", "codefresh.userToken.token", "codefresh.accountId", "runtime.name", "runtime.ingressUrl", "service.type", "service.nodePort", "app-proxy.config.cors"}
 	inputScanner := bufio.NewScanner(os.Stdin)
 	count := 1
-	for _, key := range keys {
-		fmt.Printf("%d. Enter %s (default:%s):", count, key, data[key])
-		for inputScanner.Scan() {
-			val := inputScanner.Text()
-			data[key] = val
-			break
-		}
-		count++
-	}
-	keys = []string{"codefreshNamespace", "clusterName", "environmentVariableExtractScriptPath"}
-	envVar := map[string]string{
+	keys := []string{"valuesFilePath", "codefreshNamespace", "clusterName", "environmentVariableExtractScriptPath"}
+	paramMap := map[string]string{
+		"valuesFilePath":                       "/Users/chenkeinan/workspace/codefresh-values/sandbox.values.yaml",
 		"codefreshNamespace":                   "codefresh",
 		"clusterName":                          "kind-codefresh-local-cluster",
 		"environmentVariableExtractScriptPath": "/Users/chenkeinan/workspace/codefresh-values/env.sh",
+		"debug-app-proxy":                      "y",
+		"debug-gitops-operator":                "y",
 	}
 	for _, key := range keys {
-		fmt.Printf("%d. Enter %s :", count, key)
+		fmt.Printf("%d. Enter %s (default:%s):", count, key, paramMap[key])
 		for inputScanner.Scan() {
 			input := inputScanner.Text()
-			if input == "" && len(envVar[key]) == 0 {
+			if input == "" && len(paramMap[key]) == 0 {
 				fmt.Print("you must enter a value\n")
 				fmt.Printf("%d. Enter %s", count, key)
 				continue
 			}
 			if input != "" {
-				envVar[key] = input
+				paramMap[key] = input
 			}
 			break
 		}
 		count++
 	}
+	file, err := os.ReadFile(paramMap["valuesFilePath"])
+	if err != nil {
+		panic(err.Error())
+	}
+	doc, err := chartutil.ReadValues(file)
+	if err != nil {
+		panic(err.Error())
+	}
+	userToken, err := doc.Table("global.codefresh.userToken")
+	if err != nil {
+		panic(err.Error())
+	}
+	paramMap["codefreshUserToken"] = userToken["token"].(string)
+	gitData, err := doc.Table("global.runtime.gitCredentials.password")
+	if err != nil {
+		panic(err.Error())
+	}
+	paramMap["gitPassword"] = gitData["value"].(string)
+	AddEnvParams(paramMap)
+
+	if paramMap["debug-app-proxy"] == "y" {
+		paramMap["app-proxy-local-ip"] = getNgrokPublicUrl("3017", "4040")
+	}
+
+	if paramMap["debug-app-proxy"] == "y" {
+		paramMap["gitops-operator-local-ip"] = getNgrokPublicUrl("8082", "4041")
+	}
+	KillProcess("gnrok")
+	fmt.Println(paramMap)
+}
+
+func KillProcess(name string) {
+	_, err := exec.Command("bash", "-c", "pgrep -f ngrok | xargs kill -9").Output()
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func trimValues(val string) string {
+	sepArray := []string{`\n`, `\r`, `,`, `"`, `\`}
+	for _, sep := range sepArray {
+		val = strings.ReplaceAll(val, sep, "")
+	}
+	return strings.TrimSpace(val)
+}
+
+func getNgrokPublicUrl(port string, tunnelPort string) string {
+	var stdoutBuf, stderrBuf bytes.Buffer
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("ngrok http %s &", port))
+	cmd.Stdout = io.MultiWriter(os.Stdout, &stdoutBuf)
+	cmd.Stderr = io.MultiWriter(os.Stderr, &stderrBuf)
+	err := cmd.Start() // Starts command asynchronously
+	if err != nil {
+		panic(err.Error())
+	}
+	time.Sleep(time.Second * 2)
+	res, err := http.Get(fmt.Sprintf("http://localhost:%s/api/tunnels", tunnelPort))
+	if err != nil {
+		panic(err.Error())
+	}
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	var tmapOne = make(map[string]interface{})
+	err = json.Unmarshal(b, &tmapOne)
+	if err != nil {
+		panic(err.Error())
+	}
+	gnroks := tmapOne["tunnels"]
+	aa := gnroks.([]interface{})
+	n := aa[0].(map[string]interface{})
+	return n["public_url"].(string)
+}
+
+func AddEnvParams(envVar map[string]string) {
 	cmd := fmt.Sprintf("%s %s %s", envVar["environmentVariableExtractScriptPath"], envVar["codefreshNamespace"], envVar["clusterName"])
-	fmt.Print(cmd)
 	out, err := exec.Command("bash", "-c", cmd).Output()
 	if err != nil {
-		panic("some error found")
+		panic(err.Error())
 	}
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(0)
+		panic(err.Error())
 	}
-	fmt.Print(string(out))
-	//file := strings.NewReader(string(out))
-	//scanner := bufio.NewScanner(file)
-	//fmt.Println(string(out))
+	env := string(out)
+	envScanner := bufio.NewScanner(strings.NewReader(env))
+	for envScanner.Scan() {
+		input := envScanner.Text()
+		values := strings.Split(input, ":")
+		if len(values) == 2 {
+			envVar[trimValues(values[0])] = trimValues(values[1])
+		}
+	}
 }
