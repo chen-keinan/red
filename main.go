8dc253754fa850cbff3ee1f51391aa25c8e0c36c
Chen Keinan <hen.keinan@gmail.com>
2025-02-10 10:27:33 +0200

    update cm dev values
    Signed-off-by: Chen Keinan <hen.keinan@gmail.com>

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"os/user"
 	"strings"
 	"time"
 
@@ -21,6 +22,7 @@ func main() {
 		cleanup()
 		return
 	}
+	cleanup()
 	paramMap := map[string]string{
 		"valuesFilePath":                       "/Users/chenkeinan/workspace/codefresh-values/sandbox.values.yaml",
 		"codefreshNamespace":                   "codefresh",
@@ -52,9 +54,112 @@ func main() {
 			fmt.Println("setting port forward 2746")
 			portForward("2746", "2746", "argo-server")
 		}
+		fmt.Println("scalling gitops operator down to 0")
 		patchGitOpsDeployment()
+		fmt.Println("updating gitops-operator-notifications cm with gitops local dev ip")
 		patchConfigMap("gitops-operator-notifications-cm", "service.webhook.cf-promotion-app-degraded-notifier", fmt.Sprintf("url: %s/app-degraded", paramMap["gitops-operator-local-ip"]))
 		patchConfigMap("gitops-operator-notifications-cm", "service.webhook.cf-promotion-app-revision-changed-notifier", fmt.Sprintf("url: %s/app-revision-changed", paramMap["gitops-operator-local-ip"]))
+	}
+	outputFolder := getOutputFolder()
+	createOutputFolder(outputFolder)
+	generateEnvVarForGitOpsOpertorDev(paramMap, outputFolder)
+	generateEnvVarForAppProxyDev(paramMap, outputFolder)
+}
+
+func getOutputFolder() string {
+	usr, err := user.Current()
+	if err != nil {
+		panic(err.Error())
+	}
+	return fmt.Sprintf("%s/dev-output", usr.HomeDir)
+}
+
+func createOutputFolder(path string) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, os.ModePerm)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+}
+
+func generateEnvVarForGitOpsOpertorDev(paramMap map[string]string, outputFolder string) {
+	gitOpsOperatorMap := map[string]string{
+		"AP_URL":                    "<app-proxy-local-ip>",
+		"ARGO_CD_URL":               "localhost:8081",
+		"ARGO_WF_URL":               "http://localhost:2746",
+		"ARGO_WF_TOKEN":             "ARGO_WF_TOKEN",
+		"CF_TOKEN":                  "RUNTIME_TOKEN",
+		"CF_URL":                    "CF_HOST",
+		"GITOPS_OPERATOR_VERSION":   "v0.3.17",
+		"HEALTH_PROBE_BIND_ADDRESS": ":8081",
+		"LEADER_ELECT":              "false",
+		"METRICS_BIND_ADDRESS":      "127.0.0.1: 8085",
+		"RUNTIME":                   "codefresh",
+	}
+	gitOpsOperatorMap["AP_URL"] = paramMap["app-proxy-local-ip"]
+	gitOpsOperatorMap["ARGO_WF_TOKEN"] = paramMap["ARGO_WF_TOKEN"]
+	gitOpsOperatorMap["CF_TOKEN"] = paramMap["RUNTIME_TOKEN"]
+	gitOpsOperatorMap["CF_URL"] = paramMap["CF_HOST"]
+
+	gitOpsOperatorData, err := json.MarshalIndent(gitOpsOperatorMap, "", "    ")
+	if err != nil {
+		panic(err.Error())
+	}
+	err = os.WriteFile(fmt.Sprintf("%s/gitops-dev-env.json", outputFolder), gitOpsOperatorData, 0755)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func generateEnvVarForAppProxyDev(paramMap map[string]string, outputFolder string) {
+	appProxyMap := map[string]string{
+		"NODE_TLS_REJECT_UNAUTHORIZED": "0",
+		"ARGO_CD_URL":                  "http://localhost:8089",
+		"ARGO_WORKFLOWS_URL":           "http://localhost:2746",
+		"GIT_PASSWORD":                 "gitPassword",
+		"ARGO_CD_PASSWORD":             "ARGO_CD_PASSWORD",
+		"ARGO_CD_USERNAME":             "admin",
+		"ARGO_WORKFLOWS_INSECURE":      "true",
+		"ARGO_WORKFLOWS_SA_TOKEN":      "ARGO_WORKFLOWS_SA_TOKEN",
+		"CF_HOST":                      "CF_HOST",
+		"CHART_VERSION":                "0.15.0",
+		"CLUSTER":                      "https://kubernetes.default.svc",
+		"CORS":                         "cors",
+		"DEPLOYMENT_NAME":              "cap-app-proxy",
+		"HELM_RELEASE_NAME":            "cf-gitops-runtime",
+		"INSTALLATION_TYPE":            "HELM",
+		"LOG_LEVEL":                    "info",
+		"MANAGED":                      "false",
+		"NAMESPACE":                    "codefresh",
+		"PART_OF_VALUE":                "app-proxy",
+		"ROLLOUTS_HELM_REPOSITORY":     "https://codefresh-io.github.io/argo-helm",
+		"ROLLOUTS_HELM_VERSION":        "2.37.3-1-v1.7.1-CR-24605",
+		"RUNTIME_NAME":                 "gitlab-runtime",
+		"RUNTIME_STORE_IV":             "RUNTIME_STORE_IV",
+		"RUNTIME_TOKEN":                "RUNTIME_TOKEN",
+		"RUNTIME_VERSION":              "0.1.65",
+		"SKIP_PERMISSIONS_VALIDATION":  "false",
+		"USER_TOKEN":                   "USER_TOKEN",
+	}
+	appProxyMap["GIT_PASSWORD"] = paramMap["gitPassword"]
+	appProxyMap["ARGO_CD_USERNAME"] = paramMap["ARGO_CD_USERNAME"]
+	appProxyMap["ARGO_CD_PASSWORD"] = paramMap["ARGO_CD_PASSWORD"]
+	appProxyMap["ARGO_WORKFLOWS_SA_TOKEN"] = paramMap["ARGO_WORKFLOWS_SA_TOKEN"]
+	appProxyMap["CF_HOST"] = paramMap["CF_HOST"]
+	appProxyMap["RUNTIME_NAME"] = paramMap["RUNTIME_NAME"]
+	appProxyMap["RUNTIME_STORE_IV"] = paramMap["RUNTIME_STORE_IV"]
+	appProxyMap["RUNTIME_TOKEN"] = paramMap["RUNTIME_TOKEN"]
+	appProxyMap["USER_TOKEN"] = paramMap["USER_TOKEN"]
+	appProxyMap["CORS"] = paramMap["cors"]
+
+	gitAppProxyData, err := json.MarshalIndent(appProxyMap, "", "    ")
+	if err != nil {
+		panic(err.Error())
+	}
+	err = os.WriteFile(fmt.Sprintf("%s/app-proxy-dev-env.json", outputFolder), gitAppProxyData, 0755)
+	if err != nil {
+		panic(err.Error())
 	}
 }
 
@@ -160,6 +265,16 @@ func AddHelmValues(paramMap map[string]string) {
 		panic(err.Error())
 	}
 	paramMap["gitPassword"] = gitData["value"].(string)
+	corsData, err := doc.Table("app-proxy.config")
+	if err != nil {
+		panic(err.Error())
+	}
+	paramMap["cors"] = corsData["cors"].(string)
+	hostData, err := doc.Table("global.codefresh")
+	if err != nil {
+		panic(err.Error())
+	}
+	paramMap["CF_HOST"] = hostData["url"].(string)
 }
 
 func readInput(paramMap map[string]string) {
